3ba32463577cde559db820bb02b6e6f24ec3b78c
Lexie <ff12817@gmail.com>
2024-03-14 20:10:35 +0800

    test standings function

--- index.py
+++ index.py
@@ -43,21 +43,19 @@ def handle_message(event):
     elif textSendByUser == "聯盟戰績":
         state = get_standings()
         if state == "OK":
-            East_df = pd.read_csv("data/eastStandings.csv")
-            if not East_df.empty:
-                message = "yaya"
-                #     message += "東區戰績\n"
-                #     for index, row in East_df.iterrows():
-                #         message += f"{index+1}. {row['球隊名稱']} {row['戰績']}\n"
+            # East_df = pd.read_csv("data/eastStandings.csv")
+            # if not East_df.empty:
+            message = "yaya"
+            #     message += "東區戰績\n"
+            #     for index, row in East_df.iterrows():
+            #         message += f"{index+1}. {row['球隊名稱']} {row['戰績']}\n"
 
-                # West_df = pd.read_csv("data/westStandings.csv")
-                # if not West_df.empty:
-                #     message += "西區戰績\n"
-                #     for index, row in West_df.iterrows():
-                #         message += f"{index+1}. {row['球隊名稱']} {row['戰績']}\n"
-                line_bot_api.reply_message(
-                    event.reply_token, TextSendMessage(text=message)
-                )
+            # West_df = pd.read_csv("data/westStandings.csv")
+            # if not West_df.empty:
+            #     message += "西區戰績\n"
+            #     for index, row in West_df.iterrows():
+            #         message += f"{index+1}. {row['球隊名稱']} {row['戰績']}\n"
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
         else:
             message = "處理失敗 請重新輸入"
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
