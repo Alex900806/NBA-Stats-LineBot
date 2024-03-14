13be6e75c8bda59a06c86ba76e34d16446c0f6ee
Lexie <ff12817@gmail.com>
2024-03-14 22:05:02 +0800

    test standings function

--- index.py
+++ index.py
@@ -41,7 +41,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 
     elif textSendByUser == "聯盟戰績":
-        message = handle_standings_request()
+        message = "聯盟戰績"
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 
     else:
