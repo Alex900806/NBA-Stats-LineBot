72790f948d92b3af75a1e27dfeb24633acf6d45e
Lexie <ff12817@gmail.com>
2024-03-15 13:49:58 +0800

    test standings function

--- standings.py
+++ standings.py
@@ -17,26 +17,12 @@ def get_standings():
         teamInfo = {}
         if standings[5] == "East":
             teamInfo["球隊名稱"] = str(standings[3]) + " " + str(standings[4])
-            teamInfo["戰績"] = (
-                str(standings[12])
-                + "-"
-                + str(standings[13])
-                + " ("
-                + (str(standings[14])[1:])
-                + ")"
-            )
+            teamInfo["戰績"] = str(standings[12]) + "-" + str(standings[13])
             eastStandings.append(teamInfo)
 
         elif standings[5] == "West":
             teamInfo["球隊名稱"] = str(standings[3]) + " " + str(standings[4])
-            teamInfo["戰績"] = (
-                str(standings[12])
-                + "-"
-                + str(standings[13])
-                + " ("
-                + (str(standings[14])[1:])
-                + ")"
-            )
+            teamInfo["戰績"] = str(standings[12]) + "-" + str(standings[13])
             westStandings.append(teamInfo)
 
     eastStandings_DF = pd.DataFrame(eastStandings).sort_values("戰績", ascending=False)
@@ -58,21 +44,21 @@ def handle_standings_request():
     t = threading.Thread(target=get_standings_async)
     t.start()
     t.join(timeout=10)  # 等待子執行緒完成，最多等待10秒
-    message = ""
+    message = "現在戰績狀況如下：\n"
     East_df = pd.read_csv("data/eastStandings.csv")
     if not East_df.empty:
-        message += "東區戰績\n"
+        message += "----------東區戰績----------\n"
         for index, row in East_df.iterrows():
             rank = f"{index+1}.".ljust(3)  # 將排名左對齊並填充空格至長度3
-            team = row["球隊名稱"].ljust(23)  # 將球隊名稱左對齊並填充空格至長度20
+            team = row["球隊名稱"].ljust(22)  # 將球隊名稱左對齊並填充空格至長度20
             record = row["戰績"]  # 戰績保持原樣
             message += f"{rank} {team} {record}\n"
         West_df = pd.read_csv("data/westStandings.csv")
     if not West_df.empty:
-        message += "\n西區戰績\n"
+        message += "\n----------西區戰績----------\n"
         for index, row in West_df.iterrows():
             rank = f"{index+1}.".ljust(3)  # 將排名左對齊並填充空格至長度3
-            team = row["球隊名稱"].ljust(23)  # 將球隊名稱左對齊並填充空格至長度20
+            team = row["球隊名稱"].ljust(22)  # 將球隊名稱左對齊並填充空格至長度20
             record = row["戰績"]  # 戰績保持原樣
             message += f"{rank} {team} {record}\n"
     return message
