09f60340af69c887a7ffe8dd04134738f0040df6
Lexie <ff12817@gmail.com>
2024-03-20 15:26:14 +0800

    test async function

--- index.py
+++ index.py
@@ -39,7 +39,7 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+async def handle_message(event):
     textSendByUser = event.message.text  # 獲取使用者傳遞的訊息
 
     if textSendByUser == "使用指南":
@@ -63,7 +63,7 @@ def handle_message(event):
         #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 
         playerName = textSendByUser[4:]
-        link = asyncio.run(main(playerName))
+        link = await main(playerName)
 
         if link != "Failed":
             image_message = ImageSendMessage(
